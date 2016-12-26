cf3181188fdfaf1afa3ce0369874404048d105f0
rawan <rhamad@dopravo.com>
2016-12-26 13:43:31 +0300

    init app architecture

---
+++ app/src/main/java/com/rawan/android/shoppinglist/utils/Constants.java
@@ -0,0 +1,33 @@
+package com.rawan.android.shoppinglist.utils;
+
+/**
+ * Created by Rawan on 12/26/16.
+ */
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+    /**
+     * Constants related to locations in Firebase, such as the name of the node
+     * where active lists are stored (ie "activeLists")
+     */
+
+
+    /**
+     * Constants for Firebase object properties
+     */
+
+
+    /**
+     * Constants for Firebase URL
+     */
+
+
+    /**
+     * Constants for bundles, extras and shared preferences keys
+     */
+
+
+}
