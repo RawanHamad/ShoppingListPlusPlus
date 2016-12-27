591c0657969fad422b9f90908441843fd4e6d594
rawan <rhamad@dopravo.com>
2016-12-27 15:06:25 +0300

    Debugging with Log Level Debug

--- app/src/main/java/com/rawan/android/shoppinglist/ShoppingListApplication.java
+++ app/src/main/java/com/rawan/android/shoppinglist/ShoppingListApplication.java
@@ -3,6 +3,7 @@ package com.rawan.android.shoppinglist;
 import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Logger;
 
 /**
  * Created by Rawan on 12/27/16.
@@ -16,6 +17,8 @@ public class ShoppingListApplication extends Application {
     public void onCreate() {
         super.onCreate();
         database = FirebaseDatabase.getInstance();
+        FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
+
 
 
     }
