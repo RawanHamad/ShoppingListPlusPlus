caf12bdef9c9ac6c50195b20bff3a9f28564733f
rawan <rhamad@dopravo.com>
2016-12-27 10:00:40 +0300

    writing to firebase database

---
+++ app/src/main/java/com/rawan/android/shoppinglist/ShoppingListApplication.java
@@ -0,0 +1,15 @@
+package com.rawan.android.shoppinglist;
+
+import android.app.Application;
+
+/**
+ * Created by Rawan on 12/27/16.
+ */
+
+public class ShoppingListApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
