7cf97c375da1c61f66e7d7eeecabfc7c2587efe5
rawan <rhamad@dopravo.com>
2017-01-14 12:34:23 +0300

    create account with email and password

---
+++ app/src/main/java/com/rawan/android/shoppinglist/ui/login/CreateAccountActivity.java
@@ -0,0 +1,189 @@
+package com.rawan.android.shoppinglist.ui.login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rawan.android.shoppinglist.R;
+import com.rawan.android.shoppinglist.ui.BaseActivity;
+import com.rawan.android.shoppinglist.utils.Constants;
+
+
+/**
+ * Created by Rawan on 1/5/17.
+ */
+
+public class CreateAccountActivity extends BaseActivity {
+    private static final String LOG_TAG = CreateAccountActivity.class.getSimpleName();
+    private ProgressDialog mAuthProgressDialog;
+    private EditText mEditTextUsernameCreate, mEditTextEmailCreate, mEditTextPasswordCreate;
+    private DatabaseReference mFirebaseRef;
+    private FirebaseAuth mAuth;
+
+    private String mUserName, mUserEmail, mPassword;
+    private static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        /**
+         * Create Firebase references
+         */
+        mFirebaseRef = FirebaseDatabase.getInstance().getReferenceFromUrl(Constants.FIREBASE_URL);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        /**
+         * Link layout elements from XML and setup the progress dialog
+         */
+        initializeScreen();
+    }
+
+    /**
+     * Override onCreateOptionsMenu to inflate nothing
+     *
+     * @param menu The menu with which nothing will happen
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
+    /**
+     * Link layout elements from XML and setup the progress dialog
+     */
+    public void initializeScreen() {
+        mEditTextUsernameCreate = (EditText) findViewById(R.id.edit_text_username_create);
+        mEditTextEmailCreate = (EditText) findViewById(R.id.edit_text_email_create);
+        mEditTextPasswordCreate = (EditText) findViewById(R.id.edit_text_password_create);
+        LinearLayout linearLayoutCreateAccountActivity = (LinearLayout) findViewById(R.id.linear_layout_create_account_activity);
+        initializeBackground(linearLayoutCreateAccountActivity);
+
+        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle(getResources().getString(R.string.progress_dialog_loading));
+        mAuthProgressDialog.setMessage(getResources().getString(R.string.progress_dialog_creating_user_with_firebase));
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    /**
+     * Open LoginActivity when user taps on "Sign in" textView
+     */
+    public void onSignInPressed(View view) {
+        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    /**
+     * Create new account using Firebase email/password provider
+     */
+    public void onCreateAccountPressed(View view) {
+
+
+        mUserName = mEditTextUsernameCreate.getText().toString();
+        mUserEmail = mEditTextEmailCreate.getText().toString().toLowerCase();
+        mPassword = mEditTextPasswordCreate.getText().toString();
+
+        /**
+         * Check that email and user name are okay
+         */
+        boolean validEmail = isEmailValid(mUserEmail);
+        boolean validUserName = isUserNameValid(mUserName);
+        boolean validPassword = isPasswordValid(mPassword);
+        if (!validEmail || !validUserName || !validPassword) return;
+
+        /**
+         * If everything was valid show the progress dialog to indicate that
+         * account creation has started
+         */
+        mAuthProgressDialog.show();
+
+        /**
+         * Create new user with specified email and password
+         */
+
+
+        mAuth.createUserWithEmailAndPassword(mUserEmail, mPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        mAuthProgressDialog.dismiss();
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(LOG_TAG, getString(R.string.log_error_occurred) + task.getException().toString());
+                          //  mEditTextEmailCreate.setError(task.getException().toString());
+                            showErrorToast(task.getException().toString());
+
+                        } else {
+                            Log.i(LOG_TAG, getString(R.string.log_message_auth_successful));
+
+                        }
+
+                    }
+                });
+
+    }
+
+    /**
+     * Creates a new user in Firebase from the Java POJO
+     */
+    private void createUserInFirebaseHelper(final String encodedEmail) {
+    }
+
+    private boolean isEmailValid(String email) {
+
+        boolean isGoodEmail = (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEditTextEmailCreate.setError(String.format(getString(R.string.error_invalid_email_not_valid),
+                    email));
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isUserNameValid(String userName) {
+
+        if (userName.equals("")) {
+            mEditTextUsernameCreate.setError(getResources().getString(R.string.error_cannot_be_empty));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPasswordValid(String password) {
+        if (password.length() < 6) {
+            mEditTextPasswordCreate.setError(getResources().getString(R.string.error_invalid_password_not_valid));
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Show error toast to users
+     */
+    private void showErrorToast(String message) {
+        Toast.makeText(CreateAccountActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+}
